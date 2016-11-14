aa7b3273f3cb413cb6c4695237e73e6975a076e6
Odinokov Nikolay <zyamlik@mail.ru>
2016-11-15 01:55:50 +0300

    [QA] Task issue

---
+++ src/ru/storage/notes/Practice.java
@@ -0,0 +1,19 @@
+package ru.storage.notes;
+
+public class Practice { 
+    public static void main(String[] args) {
+    	byte a = 2;
+    	byte b = 2;
+    	System.out.println(a*b);
+    	Integer i1 = 100;
+    	Integer i2 = 100;
+    	System.out.println(i1.equals(i2));
+    	System.out.println(i1.equals(100));
+    	int i = 012;
+    	System.out.println(i);
+    	
+    	if (args instanceof Object) {
+    		System.out.println("hello");
+    	}
+    }
+} 
